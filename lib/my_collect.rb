d62e43533ef9ab1643091577821bc2842847c71d
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-23 06:00:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(collection)
   i = 0
-  collection = []
+  new_collection = []
   
   while i < collection.length
-    collection[i]
+    new_collection << yield(collection[i])
+    i += 1
   end
+  collection
 end
